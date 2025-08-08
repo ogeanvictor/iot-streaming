0cea7ea2320d485cfa92f790221c695d3069f553
Gean <geanvictorduque@gmail.com>
2025-08-07 21:00:20 -0300

    build: create mongodb instance

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1/database-iot')],
   controllers: [],
   providers: [],
 })
